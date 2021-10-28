f1d9a22c2471e5320b6ea50938975b60d9bc0922
shadmanxp <57424458+shadmanxp@users.noreply.github.com>
2021-10-29 01:06:31 +0600

    fixing file rename

--- mysite/portal/file_dal.py
+++ mysite/portal/file_dal.py
@@ -1,4 +1,6 @@
 import os
+import time
+import uuid
 from django.core.exceptions import ValidationError
 
 def get_category_list():
@@ -13,10 +15,32 @@ def get_category_list():
     choices = [tuple([each, each]) for each in category]
     return choices
 
+
 def handle_uploaded_file(f):
-    with open('static/file_upload/'+f.name, 'wb+') as destination:
-        # if destination.read(f.name):
-        #     print(f.name)
-        # else:
-            for chunk in f.chunks():
-                destination.write(chunk)
+    with open('static/file_upload/' + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+        destination.close()
+
+        filename = os.path.join(f.name, '')
+
+
+# def handle_uploaded_file(f):
+#         if os.path.isfile('static/file_upload/'+f.name):
+#
+#             rename_file = f.name.split('.')[0]+"_"+str(time.time())+f.name.split('.')[1]
+#             filename = os.path.join(f.name, '')
+#             filename = os.(f.name, rename_file)
+#             if os.path.isfile('static/file_upload/'+filename):
+#                 handle_uploaded_file(filename)
+#             with open('static/file_upload/'+filename, 'wb+') as destination:
+#                 for chunk in filename.chunks():
+#                     destination.write(chunk)
+#                 destination.close()
+#                 print(f.name)
+#         else:
+#             for chunk in f.chunks():
+#                 with open('static/file_upload/' + f.name, 'wb+') as destination:
+#                     for chunk in f.chunks():
+#                         destination.write(chunk)
+#                     destination.close()
